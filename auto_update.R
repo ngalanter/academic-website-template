a1d549aecaab0ceb2c10a53a2ea599d251de99e8
Nina Galanter <31412535+ngalanter@users.noreply.github.com>
2026-01-30 17:31:42 -0500

    small working example of auto update

--- auto_update.R
+++ auto_update.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(stringr)
+library(purrr)
 
 # formatting helpers ----------------------
 
@@ -19,9 +20,11 @@ link_f <- function(txt,link){ paste0("[",txt,"](",link,")")}
 
 paren <- function(txt){ paste0("(",txt,")") }
 
-# item helpers -------------------------------
+# item helpers ------------------------------
 
-pub <- function(authors,year,title,link=NA,journal){
+# ... at ends of functions so can apply to dataframe without selecting vars
+
+pub <- function(authors,year,title,link=NA,journal,...){
   
   ftitle <- title
   
@@ -31,3 +34,38 @@ pub <- function(authors,year,title,link=NA,journal){
   
   paste0(paste(bf_name(authors),paren(year),ftitle,it(journal), sep = ". "),".")
 }
+
+# page helpers ------------------------------
+
+cv <- function(old_cv,content){
+  
+  parts <- list(old_cv[1:(str_which(old_cv,"cv_pub_update_start")+1)],
+                old_cv[(str_which(old_cv,"cv_pub_update_start")+2):
+                       (str_which(old_cv,"cv_pub_update_end")-2)],
+                old_cv[(str_which(old_cv,"cv_pub_update_end")-1):
+                         length(old_cv)])
+  
+  update_with <- content %>% filter(type == "Publication") %>% 
+    arrange(desc(year)) %>% pmap(pub) %>% unlist()
+  
+  update_with <- c("",rbind(update_with,rep("",length(update_with))))
+  
+  parts[[2]] <- update_with
+  
+  return(do.call(c,parts))
+  
+}
+
+
+
+# run the update ------------------------------
+
+content <- read.csv("sample_content.csv")
+
+old_cv <- readLines("cv_resume1.qmd")
+
+writeLines(old_cv,"old_cv_backup.qmd")
+
+new_cv <- cv(old_cv,content)
+
+writeLines(new_cv,"cv.qmd")
